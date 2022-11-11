bb5ba881498cb047e7527c2b2e6d43a991992f71
Reza Sadeghi <83159480+staralgorithem@users.noreply.github.com>
2022-11-11 17:07:57 +0330

    Create MediaPipe_Holistic.py

---
+++ MediaPipe_Holistic.py
@@ -0,0 +1,10 @@
+#import Libraries
+import cv2
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_holistic = mp.solutions.holistic
+
+# For webcam input:
+cap = cv2.VideoCapture(0)
