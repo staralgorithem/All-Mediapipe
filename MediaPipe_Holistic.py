352815c6c48b5eb34ac2edce43f6716ff4fc0ae2
Reza Sadeghi <83159480+staralgorithem@users.noreply.github.com>
2022-11-11 17:11:04 +0330

    Update MediaPipe_Holistic.py

--- MediaPipe_Holistic.py
+++ MediaPipe_Holistic.py
@@ -7,4 +7,14 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_holistic = mp.solutions.holistic
 
 # For webcam input:
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) #To change camera capture you have to change the number
+
+with mp_holistic.Holistic(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+        success, image = cap.read()
+        w = image.shape[0]
+        h = image.shape[1]
+        
+        
