266fa85a5ce72871314a589118ebce2ef35b77ea
Reza Sadeghi <83159480+staralgorithem@users.noreply.github.com>
2022-11-12 19:29:07 +0330

    Update MediaPipe_Pose.py

--- MediaPipe_Pose.py
+++ MediaPipe_Pose.py
@@ -6,24 +6,6 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
-with mp_pose.Pose(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as pose:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = pose.process(image)
-    
-    for index, landmark in enumerate(results.pose_landmarks.landmark):
-      # val = results.pose_landmarks;
-      # num = len(results.pose_landmarks.landmark);
-      print("index : " + str(index) + " x: "+ str(landmark.x) + " y: "+ str(landmark.y) + " z: "+ str(landmark.z));
-    # print(val)
+# For webcam input:
+cap = cv2.VideoCapture(0)
