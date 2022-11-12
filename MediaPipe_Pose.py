2bf5bdf73edb6b7c00b6442c01fa0586c01db618
Reza Sadeghi <83159480+staralgorithem@users.noreply.github.com>
2022-11-12 19:26:13 +0330

    Update MediaPipe_Pose.py

--- MediaPipe_Pose.py
+++ MediaPipe_Pose.py
@@ -9,3 +9,9 @@ mp_pose = mp.solutions.pose
 with mp_pose.Pose(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as pose:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
