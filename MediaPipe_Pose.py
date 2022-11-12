39d440e91722ce6c9a9bb96981068be90b54ed26
Reza Sadeghi <83159480+staralgorithem@users.noreply.github.com>
2022-11-12 19:24:49 +0330

    Update MediaPipe_Pose.py

--- MediaPipe_Pose.py
+++ MediaPipe_Pose.py
@@ -5,3 +5,7 @@ import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
+
+with mp_pose.Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as pose:
