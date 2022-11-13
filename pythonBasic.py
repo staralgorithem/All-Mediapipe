752468af7294740a7f4c1abe55afe1ec1996c5cd
Reza Sadeghi <83159480+staralgorithem@users.noreply.github.com>
2022-11-13 16:35:39 +0330

    Update pythonBasic.py

--- pythonBasic.py
+++ pythonBasic.py
@@ -6,5 +6,10 @@ import mediapipe
 drawingModule = mediapipe.solutions.drawing_utils
 handsModule = mediapipe.solutions.hands
 
+# For webcam input:
 capture = cv2.VideoCapture(0)
+
+
+ with handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, min_tracking_confidence=0.7, max_num_hands=2) as hands:
  
+    while (True):
