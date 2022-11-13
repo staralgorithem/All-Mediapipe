940a9f9c1e299817e98fc3a85c440ac6dd52ace6
Reza Sadeghi <83159480+staralgorithem@users.noreply.github.com>
2022-11-13 16:36:20 +0330

    Update pythonBasic.py

--- pythonBasic.py
+++ pythonBasic.py
@@ -10,6 +10,10 @@ handsModule = mediapipe.solutions.hands
 capture = cv2.VideoCapture(0)
 
 
- with handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, min_tracking_confidence=0.7, max_num_hands=2) as hands:
+with handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, min_tracking_confidence=0.7, max_num_hands=2) as hands:
  
     while (True):
+ 
+        ret, frame = capture.read()
+        results = hands.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+ 
