8e906561642d59f9ca4344a97a92157c8e8c54b6
Reza Sadeghi <83159480+staralgorithem@users.noreply.github.com>
2022-11-14 12:49:50 +0330

    Update pythonBasic2.py

--- pythonBasic2.py
+++ pythonBasic2.py
@@ -8,5 +8,8 @@ mp_holistic = mp.solutions.holistic
 
 # Capture
 cap = cv2.VideoCapture(0)
-
-
+# Initiate holistic model
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    
+    while cap.isOpened():
+        ret, frame = cap.read()
